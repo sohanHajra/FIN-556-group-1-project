1db1d9537b1a7a2f38599feb048648cc89b2dfd1
Danny Silverstein <dannys2@illinois.edu>
2025-12-17 16:39:41 +0000

    revert conditionals for arb

--- src/venue_arb/venue_arb.cpp
+++ src/venue_arb/venue_arb.cpp
@@ -117,10 +117,10 @@ void venue_arb::EvaluateArb(const Instrument* inst)
 
     int desired_position = 0;
 
-    if (i.bid > 0 + arb_threshold_) {
+    if (i.bid > n.ask + arb_threshold_) {
         desired_position = position_size_;
     }
-    else if (n.bid > 0 + arb_threshold_) {
+    else if (n.bid > i.ask + arb_threshold_) {
         desired_position = -position_size_;
     }
 
