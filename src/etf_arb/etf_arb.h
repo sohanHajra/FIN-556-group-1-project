93d56e80a8e5a1197633c1e4978c46a8dd5dc2db
acharov2 <acharov2@illinois.edu>
2025-12-18 21:45:36 -0600

    Remove unecessary new keyword

--- src/etf_arb/etf_arb.h
+++ src/etf_arb/etf_arb.h
@@ -89,7 +89,7 @@ extern "C" {
         if (std::strcmp(strategyType, GetType()) == 0) {
             // Need to use conversion operator on object not pointer
             // this is to invoke IStrategy* operator
-            return new *(new etf_arb(strategyID, strategyName, groupName));
+            return *(new etf_arb(strategyID, strategyName, groupName));
         }
         return nullptr;
     }
